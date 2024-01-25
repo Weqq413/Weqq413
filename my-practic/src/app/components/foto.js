1241b272fd21cf2dba23616276a447ae62e3bbdd
Weqq413 <155451159+Weqq413@users.noreply.github.com>
2024-01-25 22:59:25 +0000

    jgouho

--- my-practic/src/app/components/foto.js
+++ my-practic/src/app/components/foto.js
@@ -7,7 +7,7 @@ function Foto (){
     const [perrito,setPerrito]=useState("/vercel.svg");
     const [estado, setEstado]=useState("esperando");
     const [habilidad, setHabilidad] = useState("habilidad");
-    const url="https://pokeapi.co/api/v2/pokemon/eevee";    
+    const url="https://pokeapi.co/api/v2/pokemon/rattata";
     useEffect(()=>{
         fetch(url)
         .then(res=>res.json())
@@ -15,14 +15,13 @@ function Foto (){
             setPerrito(data.sprites.front_default),
             setEstado(data.species.name),
             setHabilidad(data.moves[0].move.name)
-        });
-},[]);
-return(
-    <div className={style.contenedor}>
-        <h1>{estado}</h1>
-        <Image src={perrito} height={250} width={250}/>
-        <h1>{habilidad}</h1>
-    
+            });
+    },[]);
+    return(
+        <div className={style.contenedor}>
+            <h1>{estado}</h1>
+            <Image src={perrito} height={250} width={250}/>
+            <h1>{habilidad}</h1>
         </div>
     )
 }
--- my-practic/src/app/page.js
+++ my-practic/src/app/page.js
@@ -1,15 +1,19 @@
+import Image from "next/image";
 import styles from "./page.module.css";
 import Foto from "./components/foto";
 
 export default function Home() {
   return (
     <div className={styles.main}>
-      <Foto/>
-      <Foto/>
-      <Foto/>
-      <Foto/>
-      <Foto/>
-      <Foto/>
-    </div>
+    <Foto/>
+    <Foto/>
+    <Foto/>
+    <Foto/>
+    <Foto/>
+    <Foto/>
+    
+  </div>
+
   );
 }
+
